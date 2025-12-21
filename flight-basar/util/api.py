1f1b133e3a4667bd09d2dbd4464efbd21ca74adf
Muetzilla <muetz-github@gasbichler.ch>
2025-12-21 23:30:32 +0100

    resultate als liste anzeigen

--- flight-basar/util/api.py
+++ flight-basar/util/api.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
+
 
 BASE_URL = "http://api.aviationstack.com/v1"
 API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
@@ -21,4 +23,22 @@ def call_flight_api(departure_destination: str, arrival_destination: str, limit:
     r = requests.get(url, params=params)
     r.raise_for_status()
 
-    return r.json().get("data", [])
+    return filter_necessary_infos
+
+def format_time(iso_str: str) -> str:
+    return datetime.fromisoformat(iso_str.replace("Z", "+00:00")).strftime("%H:%M")
+
+def filter_necessary_infos(flights: list[dict]) -> list[dict]:
+    simplified = []
+    print("Filtering necessary infos...")
+    for f in flights:
+        simplified.append({
+            "from": f["departure"]["airport"],
+            "to": f["arrival"]["airport"],
+            "airline": f["airline"]["name"],
+            "flight": f["flight"]["iata"],
+            "departureTime": format_time(f["departure"]["scheduled"]),
+            "arrivalTime": format_time(f["arrival"]["scheduled"]),
+        })
+
+    return simplified
