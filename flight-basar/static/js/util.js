456f2ccc01e2c8864600bc5f147f8b60aa0f57bf
Muetzilla <muetz-github@gasbichler.ch>
2025-12-21 23:10:46 +0100

    get airports from dropdown

--- flight-basar/static/js/util.js
+++ flight-basar/static/js/util.js
@@ -1,12 +1,16 @@
  async function callSearchApi() {
-              try {
-                const res = await fetch('/flights/berlin/zürich', { method: 'GET' });
-                if (!res.ok) throw new Error('HTTP ' + res.status);
-                const data = await res.json();
-                alert(JSON.stringify(data, null, 2));
-              } catch (err) {
-                alert('Fehler: ' + err.message);
-              }
-            }
+     let departure_destination_selector_element = document.getElementById("departureAirport");
+     let departure_destination_selector_value = departure_destination_selector_element.value;
+     let arrival_destination_selector_element = document.getElementById("arrivalAirport");
+     let arrival_destination_selector_value = arrival_destination_selector_element.value;
+  try {
+    const res = await fetch('/flights/'+departure_destination_selector_value+'/' + arrival_destination_selector_value, { method: 'GET' });
+    if (!res.ok) throw new Error('HTTP ' + res.status);
+    const data = await res.json();
+    alert(JSON.stringify(data, null, 2));
+  } catch (err) {
+    alert('Fehler: ' + err.message);
+  }
+}
 
-            document.getElementById('searchBtn')?.addEventListener('click', callSearchApi);
+document.getElementById('searchBtn')?.addEventListener('click', callSearchApi);
