bae53eef43a6200450fe64283a0b3e1c3dc7f3e5
ismailRi <i.riahi@hotmail.com>
2026-01-30 15:02:32 +0100

    space

--- flight-basar/app.py
+++ flight-basar/app.py
@@ -135,4 +135,4 @@ def post_kontakt():
 # -------------------------------------------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5005)
